064ff0e97efde2ba10e9b390a2b2f6ea4ba59e21
Abdul Ansari <76679241+Abdul24-code@users.noreply.github.com>
2024-08-17 03:44:15 +0530

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,13 @@
-print("Hello, World!")
-print("This is Abdul Ansari")
+#print("Hello, World!")
+#print("This is Abdul Ansari")
+# List of animals
+animals = ["zebra", "lion", "tiger", "elephant", "giraffe"]
+
+# Sort the list in-place
+animals.sort()
+print("Sorted list (in-place):", animals)
+
+# Alternatively, use sorted() to get a new sorted list
+sorted_animals = sorted(["zebra", "lion", "tiger", "elephant", "giraffe"])
+print("Sorted list (new list):", sorted_animals)
+
